2075143b0d97e3fe7c7368766ec8fe2946aa17b6
BryceEvans <evans.bryce@gmail.com>
2019-05-09 22:46:18 -0600

    5 more exercises done.

--- src/cal.py
+++ src/cal.py
@@ -22,3 +22,50 @@ and does the following:
 import sys
 import calendar
 from datetime import datetime
+
+# Write a program that accepts user input of the form
+#   `calendar.py month [year]`
+# and does the following:
+#  - If the user doesn't specify any input, your program should 
+#    print the calendar for the current month. The 'datetime'
+#    module may be helpful for this.
+
+print('num of arguments:', len(sys.argv))
+print( sys.argv[0] )
+
+if len(sys.argv) == 1:
+    m = datetime.now().month
+    print(m)
+    y = datetime.now().year
+
+    # print(calendar.month(y, m))
+    # print(calendar.month(y, m, w=3, l=3))
+
+#  - If the user specifies one argument, assume they passed in a
+#    month and render the calendar for that month of the current year.
+
+elif len(sys.argv) == 2:
+    m = int(sys.argv[1])
+    y = int(sys.argv[2])
+    print(calendar.month(y, m))
+
+#  - If the user specifies two arguments, assume they passed in
+#    both the month and the year. Render the calendar for that 
+#    month and year.
+
+else:
+    exit()
+
+
+
+# else:
+#     if len(sys.argv[2]) == 6:
+#     if sys.argv[1].isdigit() and sys.argv[2][1:5].isdigit():
+#       m = int(sys.argv[1])
+#       y = int(sys.argv[2][1:5])
+#     else:
+#       format_err
+#       exit()
+#   else:
+#     format_err
+#     exit()
--- src/functions.py
+++ src/functions.py
@@ -1,12 +1,48 @@
 # Write a function is_even that will return true if the passed-in number is even.
 
 # YOUR CODE HERE
+def is_even(n):
+    if n % 2 == 0:
+        return "Even!"
+    else:
+        return "Odd"
 
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
+print(is_even(num))
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
+
+# Write a function that doubles that value of an integer
+def double(n):
+    return n*2
+
+my_int = 10
+double(my_int)
+
+# if pass by REFERECNE, my_int = 20 (changed the original)
+# if pass by VALUE, my_int = 10 (we modified a copy)
+print(my_int)
+
+# Write a function that doubles the value of all numbers in a list
+def double_list(l):
+    return [i*2 for i in l]
+
+my_list = [2, 7, 23, 11, 4, 87]
+
+print(double_list(my_list))
+print(my_list)
+
+def capitalize_name(name):
+    full_name = name.split(" ")
+    name = full_name[0][0:1].upper() + full_name[0][1:] + " " + full_name[1][0:1].upper() + full_name[1][1:]
+    # name = name[0:1].upper() + ??? + name[].upper() + name[:]
+    print(name)
+
+my_name = "bryce evans"
+capitalize_name(my_name)
+print(my_name)
 
